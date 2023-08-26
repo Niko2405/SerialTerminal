fb35dc8436ead1babaa4ddca5bc9a4e015370ba7
Niko2405 <nilskornmann@gmail.com>
2023-08-26 21:30:41 +0200

    Create Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SerialTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
